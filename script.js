5fa1bbb17e70728f3711c9612101ab686d571f25
Anna Shcherbinina <nyusya_belous@mail.ru>
2023-12-19 15:52:17 +0700

    done complicated lesson02

---
+++ script.js
@@ -0,0 +1,16 @@
+let num = 266219;
+let array = num.toString().split('');
+
+//console.log(array.map(Number));
+
+let result = 1;
+
+for (let i = 0; i < array.length; i++) {
+    result *= array[i];
+
+}
+console.log(result);
+
+result = result ** 3;
+//console.log(result);
+console.log(result.toString().slice(0, 2));
