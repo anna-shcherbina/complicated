3909f53610246c71ce83d3e2a8f2482d78f88b72
Anna Shcherbinina <nyusya_belous@mail.ru>
2024-01-03 17:30:02 +0700

    shortened the code

--- script.js
+++ script.js
@@ -3,42 +3,6 @@
 //Задание 1
 
 //способ а) через if
-
-let array = [];
-let lang = 'ru';
-
-if (lang === 'en') {
-    array = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
-        'Thursday', 'Friday', 'Saturday'];
-    console.log(array);
-} else if (lang === 'ru') {
-    array = ['Понедельник', 'Вторник', 'Среда',
-        'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-    console.log(array);
-};
-
-//способ b) через switch-case
-
-/*
-const lang = 'ru';
-const arrayEn = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
-    'Thursday', 'Friday', 'Saturday'];
-const arrayRu = ['Понедельник', 'Вторник', 'Среда',
-    'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-
-switch (true) {
-    case lang === 'en':
-        console.log(arrayEn);
-        break
-    case lang === 'ru':
-        console.log(arrayRu);
-        break
-};
-*/
-
-//способ с) через многомерный массив
-
-/*
 const array = [];
 const lang = 'en';
 
@@ -47,12 +11,37 @@ array['ru'] = ['Понедельник', 'Вторник', 'Среда',
 array['en'] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
     'Thursday', 'Friday', 'Saturday'];
 
+
+
+if (lang === 'en') {
+    console.log(array[lang]);
+} else if (lang === 'ru') {
+    console.log(array[lang]);
+};
+
+
+//способ b) через switch-case
+
+/*
+switch (true) {
+    case lang === 'en':
+        console.log(array[lang]);
+        break
+    case lang === 'ru':
+        console.log(array[lang]);
+        break
+};
+*/
+
+//способ с) через многомерный массив
+
+/*
 console.log(array[lang]);
 */
 
 //Задание 2
 
-/*
+
 let namePerson = "Александр";
 
 let post = (namePerson === "Артем")
@@ -62,4 +51,3 @@ let post = (namePerson === "Артем")
         : "Студент";
 
 console.log(post);
-*/
